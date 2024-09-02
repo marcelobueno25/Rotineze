7113117e65c773d8e1de2dd4263806f31669d14c
marcelobueno25 <marcelobueno25@hotmail.com>
2024-09-02 14:45:33 -0300

    adicionado grafico

--- src/screens/Grafico/index.js
+++ src/screens/Grafico/index.js
@@ -1,10 +1,60 @@
 import React from "react";
 import { View } from "react-native";
+import { Card, Text, Divider } from "react-native-paper";
+import { useSelector } from "react-redux";
 
 export const Grafico = () => {
+  const habits = useSelector((state) => state.habits.habits);
+
   return (
     <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    ></View>
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 10,
+        gap: 10,
+        flexWrap: "wrap",
+      }}
+    >
+      <View style={{ width: "100%" }}>
+        <Text variant="headlineSmall">Gráfico</Text>
+      </View>
+      <View style={{ width: "100%" }}>
+        <Card>
+          <Card.Content>
+            <Text variant="titleLarge">Nome Habito</Text>
+            <Text variant="bodyMedium">{habits[0].name}</Text>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Card>
+          <Card.Content>
+            <Text variant="titleLarge">Criado</Text>
+            <Text variant="bodyMedium">{habits[0].criado}</Text>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Card>
+          <Card.Content>
+            <Text variant="titleLarge">Concluídos</Text>
+            <Text variant="bodyMedium">{habits[0].completedDates.length}</Text>
+          </Card.Content>
+        </Card>
+      </View>
+      <View style={{ width: "100%", marginVertical: 10 }}>
+        <Divider bold={true} />
+      </View>
+      <View style={{ width: "100%" }}>
+        <Card>
+          <Card.Content>
+            <Text variant="titleLarge">Descrição</Text>
+            <Text variant="bodyMedium">{habits[0].description}</Text>
+          </Card.Content>
+        </Card>
+      </View>
+    </View>
   );
 };
